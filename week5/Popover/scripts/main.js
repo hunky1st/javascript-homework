83a32744e738d3b91dd0aafb70d7745686bf45c9
hunky1st <hunky1st@naver.com>
2025-07-13 06:16:06 +0900

    refactor: 팝오버 로직 함수화 및 이벤트 위임 적용

--- week5/Popover/scripts/main.js
+++ week5/Popover/scripts/main.js
@@ -1,131 +1,81 @@
-{
-  const popoverTrigger = document.querySelector(".popover-trigger");
-  const popover = document.querySelector(".popover");
+const POPOVER_SPACE = 10;
 
-  if (popoverTrigger && popover) {
-    const triggerRect = popoverTrigger.getBoundingClientRect();
-    const popoverRect = popover.getBoundingClientRect();
-
-    const triggerCenterPosition = (triggerRect.left + triggerRect.right) / 2;
-    const popoverLeftPosition = triggerCenterPosition - popoverRect.width / 2;
-
-    const POPOVER_SPACE = 10;
-    const popoverTopPosition =
-      triggerRect.top - popoverRect.height - POPOVER_SPACE;
-
-    popover.style.left = `${popoverLeftPosition}px`;
-    popover.style.top = `${popoverTopPosition}px`;
-
-    popover.setAttribute("hidden", "true");
-
-    popoverTrigger.addEventListener("click", () => {
-      if (popover.hasAttribute("hidden")) {
-        popover.removeAttribute("hidden");
-      } else {
-        popover.setAttribute("hidden", "true");
-      }
-    });
-
-    document.addEventListener("click", ({ target }) => {
-      if (target.closest(".popover") || target.closest(".popover-trigger"))
-        return;
-      popover.setAttribute("hidden", true);
-    });
-  }
-}
-{
-  const [, popoverTrigger] = document.querySelectorAll(".popover-trigger");
-  const [, popover] = document.querySelectorAll(".popover");
+function calculatePopoverPosition(popoverTrigger, popover) {
   const triggerRect = popoverTrigger.getBoundingClientRect();
-  console.log(triggerRect.left);
-  const POPOVER_SPACE = 10;
-
   const popoverRect = popover.getBoundingClientRect();
+  const { position } = popover.dataset;
+  const triggerXCenterPosition = (triggerRect.left + triggerRect.right) / 2;
+  const triggerYCenterPosition = (triggerRect.top + triggerRect.bottom) / 2;
 
-  const popoverLeftPosition =
-    triggerRect.left - popoverRect.width - POPOVER_SPACE;
-  popover.style.setProperty("left", `${popoverLeftPosition}px`);
-  const triggerCenterPosition = (triggerRect.top + triggerRect.bottom) / 2;
-  const popoverTopPosition = triggerCenterPosition - popoverRect.height / 2;
-  popover.style.setProperty("top", `${popoverTopPosition}px`);
+  switch (position) {
+    case "top":
+      return {
+        left: triggerXCenterPosition - popoverRect.width / 2,
+        top: triggerRect.top - popoverRect.height - POPOVER_SPACE,
+      };
+    case "bottom":
+      return {
+        left: triggerXCenterPosition - popoverRect.width / 2,
+        top: triggerRect.bottom + POPOVER_SPACE,
+      };
+    case "left":
+      return {
+        left: triggerRect.left - popoverRect.width - POPOVER_SPACE,
+        top: triggerYCenterPosition - popoverRect.height / 2,
+      };
+    case "right":
+      return {
+        left: triggerRect.right + POPOVER_SPACE,
+        top: triggerYCenterPosition - popoverRect.height / 2,
+      };
+  }
+}
+
+function hide(popover) {
   popover.setAttribute("hidden", "true");
-  popoverTrigger.addEventListener("click", () => {
-    if (popover.hasAttribute("hidden")) {
-      popover.removeAttribute("hidden");
-    } else {
-      popover.setAttribute("hidden", "true");
+}
+
+function show(popover) {
+  popover.removeAttribute("hidden");
+}
+
+function isShow(popover) {
+  return !popover.hasAttribute("hidden");
+}
+
+const popoverTriggers = document.querySelectorAll(".popover-trigger");
+
+popoverTriggers.forEach((popoverTrigger) => {
+  const selector = `[data-id="${popoverTrigger.dataset.target}"]`;
+  const popover = document.querySelector(selector);
+
+  if (popover) {
+    const { left, top } = calculatePopoverPosition(popoverTrigger, popover);
+    popover.style.left = `${left}px`;
+    popover.style.top = `${top}px`;
+    hide(popover);
+  }
+});
+
+document.addEventListener("click", ({ target }) => {
+  const popoverTrigger = target.closest(".popover-trigger");
+
+  if (popoverTrigger) {
+    const selector = `[data-id="${popoverTrigger.dataset.target}"]`;
+    const popover = document.querySelector(selector);
+    const isVisible = isShow(popover);
+
+    document.querySelectorAll(".popover").forEach(hide);
+
+    if (!isVisible) {
+      show(popover);
     }
-  });
-
-  document.addEventListener("click", ({ target }) => {
-    if (target.closest(".popover") || target.closest(".popover-trigger"))
-      return;
-    popover.setAttribute("hidden", "true");
-  });
-}
-{
-  const [, , popoverTrigger] = document.querySelectorAll(".popover-trigger");
-  const [, , popover] = document.querySelectorAll(".popover");
-
-  if (popoverTrigger && popover) {
-    const triggerRect = popoverTrigger.getBoundingClientRect();
-    const popoverRect = popover.getBoundingClientRect();
-    const triggerYCenter = (triggerRect.top + triggerRect.bottom) / 2;
-    const POPOVER_SPACE = 10;
-
-    const popoverLeftPosition = triggerRect.right + POPOVER_SPACE;
-    const popoverTopPosition = triggerYCenter - popoverRect.height / 2;
-
-    popover.style.left = `${popoverLeftPosition}px`;
-    popover.style.top = `${popoverTopPosition}px`;
-    popover.setAttribute("hidden", "true");
-
-    popoverTrigger.addEventListener("click", () => {
-      if (popover.hasAttribute("hidden")) {
-        popover.removeAttribute("hidden");
-      } else {
-        popover.setAttribute("hidden", "true");
-      }
-    });
-
-    document.addEventListener("click", ({ target }) => {
-      if (target.closest(".popover") || target.closest(".popover-trigger"))
-        return;
-      popover.setAttribute("hidden", true);
-    });
+    return;
   }
-}
 
-// 아래쪽(Bottom) 팝오버
-{
-  const [, , , popoverTrigger] = document.querySelectorAll(".popover-trigger");
-  const [, , , popover] = document.querySelectorAll(".popover");
-
-  if (popoverTrigger && popover) {
-    const triggerRect = popoverTrigger.getBoundingClientRect();
-    const popoverRect = popover.getBoundingClientRect();
-    const triggerXCenter = (triggerRect.left + triggerRect.right) / 2;
-    const POPOVER_SPACE = 10;
-
-    const popoverLeftPosition = triggerXCenter - popoverRect.width / 2;
-    const popoverTopPosition = triggerRect.bottom + POPOVER_SPACE;
-
-    popover.style.left = `${popoverLeftPosition}px`;
-    popover.style.top = `${popoverTopPosition}px`;
-    popover.setAttribute("hidden", "true");
-
-    popoverTrigger.addEventListener("click", () => {
-      if (popover.hasAttribute("hidden")) {
-        popover.removeAttribute("hidden");
-      } else {
-        popover.setAttribute("hidden", "true");
-      }
-    });
-
-    document.addEventListener("click", ({ target }) => {
-      if (target.closest(".popover") || target.closest(".popover-trigger"))
-        return;
-      popover.setAttribute("hidden", true);
-    });
+  if (target.closest(".popover")) {
+    return;
   }
-}
+
+  document.querySelectorAll(".popover").forEach(hide);
+});
